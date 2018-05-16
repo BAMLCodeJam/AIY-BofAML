66a462d25fc7b65bf52cbf8b2638577459ba1754
Ian Salmons <ian.salmons@gmail.com>
2018-05-16 10:03:24 +0100

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-git pull || git clone https://github.com/BAMLCodeJam/AIY-BofAML.git .
+function clonerepo() {
+  git init
+  git remote add origin "$1"
+  git fetch
+  git reset origin/master  # this is required if files in the non-empty directory are in the repo
+  git branch -u origin/master
+}
+
+git -C . pull || clonerepo 'https://github.com/BAMLCodeJam/AIY-BofAML.git'
